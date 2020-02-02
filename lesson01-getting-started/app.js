22e02ba74238d2676fa41aeaa0e52a6d566f8d0e
Ubuntu <ubuntu@ip-172-31-20-181.us-east-2.compute.internal>
2020-02-02 16:41:15 +0000

    Complete lesson 01

--- lesson01-getting-started/app.js
+++ lesson01-getting-started/app.js
@@ -2,5 +2,10 @@ new Vue({
     el: "#app",
     data: {
         text: "Hello, world!"
+    },
+    methods: {
+        changeText: function(event) {
+            this.text = event.target.value;
+        }
     }
 });
